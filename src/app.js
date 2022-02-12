d96569859ba7fbcc985f037593a8c2e082b7fd0e
laylaloots <95276503+laylaloots@users.noreply.github.com>
2022-02-12 22:27:06 +1300

    Add: Forecast data in JS + Add: Minor CSS changes
    Removed HTML from index and add to JS file.
    
    Add in forecast data into JS
    
    Minor tweaks to CSS

--- src/app.js
+++ src/app.js
@@ -39,8 +39,45 @@ function changeCityName(event) {
   temperatureDisplay(cityName);
 }
 
+function formatDay (timestamp) {
+let date = new Date(timestamp *1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+return days[day];
+}
+
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  
+  forecast.forEach(function (forecastDay, index) {
+    if  (index < 5) {
+    forecastHTML =
+      forecastHTML +
+      `
+       <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          alt=""
+          width="50"
+        />
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span> |
+           <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+        </div>
+       </div>`;
+    }
+  });
+ 
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
